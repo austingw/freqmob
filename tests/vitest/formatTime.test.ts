3aa8cf7e07354bf1494690d16c68e5e8cdb7cbf6
austingw <senortinman@gmail.com>
2024-02-12 20:06:40 -0500

    Add test for util

---
+++ tests/vitest/formatTime.test.ts
@@ -0,0 +1,14 @@
+import { expect, test } from "vitest";
+
+// Import the function to test
+import formatTime from "../../src/utils/formatTime";
+
+test("formatTime", () => {
+  expect(formatTime(90)).toBe("1:30");
+  expect(formatTime(600)).toBe("10:00");
+  expect(formatTime(61)).toBe("1:01");
+  expect(formatTime(3600)).toBe("60:00");
+  expect(formatTime(90.4)).toBe("1:30");
+  expect(formatTime(90.5)).toBe("1:31");
+  expect(formatTime(9)).toBe("0:09");
+});

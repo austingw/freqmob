8e4a62d3f4de8c1ad1f8810f4b34dafa4a841a9b
austingw <senortinman@gmail.com>
2024-02-19 21:58:02 -0500

    Add s3 bucket to project

--- sst.config.ts
+++ sst.config.ts
@@ -1,5 +1,5 @@
 import { SSTConfig } from "sst";
-import { NextjsSite } from "sst/constructs";
+import { Bucket, NextjsSite } from "sst/constructs";
 
 export default {
   config(_input) {
@@ -10,7 +10,11 @@ export default {
   },
   stacks(app) {
     app.stack(function Site({ stack }) {
-      const site = new NextjsSite(stack, "site");
+      const bucket = new Bucket(stack, "public");
+
+      const site = new NextjsSite(stack, "site", {
+        bind: [bucket],
+      });
 
       stack.addOutputs({
         SiteUrl: site.url,

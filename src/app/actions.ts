0f44340d99fb5af60d1cc3f8e5eae5664666f221
austingw <senortinman@gmail.com>
2024-02-22 18:13:28 -0500

    Get that thang on S3 finally 🙌🙌🙌

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -1,5 +1,21 @@
 "use server";
 
-export const createPost = async (prevState: any, post: any) => {
-  console.log("post", post);
+import { getPresignedUrl } from "@/utils/getPresignedUrl";
+
+export const createPost = async (prevState: any, post: FormData) => {
+  const title = post.get("title");
+  const file = post.get("file");
+
+  if (file) {
+    const presignedUrl = await getPresignedUrl();
+    const res = await fetch(presignedUrl, {
+      method: "PUT",
+      body: file,
+      headers: {
+        "Content-Type": "audio/mp3", //to be dynamic based on file type
+        "Content-Disposition": `attachment; filename="${title}"`,
+      },
+    });
+    const audioUrl = res.url.split("?")[0];
+  }
 };

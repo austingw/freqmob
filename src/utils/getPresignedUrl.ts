9d72152741f6da2709c038b5fb8fe87a9f980ad1
austingw <senortinman@gmail.com>
2024-02-22 18:12:31 -0500

    Add util for getting presigned url from S3

---
+++ src/utils/getPresignedUrl.ts
@@ -0,0 +1,14 @@
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
+
+export const getPresignedUrl = async () => {
+  const command = new PutObjectCommand({
+    ACL: "public-read",
+    Key: crypto.randomUUID(),
+    Bucket: process.env.S3_BUCKET,
+  });
+
+  const url = await getSignedUrl(new S3Client({}), command);
+
+  return url;
+};

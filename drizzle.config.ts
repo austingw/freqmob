6aa36913901b08c4fb21cfa1677591727d2afbdc
austingw <senortinman@gmail.com>
2024-02-24 23:34:15 -0500

    Make drizzle-kit work with env

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from "drizzle-kit";
-import * as dotenv from "dotenv";
-dotenv.config();
+import dotenv from "dotenv";
+
+dotenv.config({
+  path: "./.env.local",
+});
 
 export default defineConfig({
   schema: "./src/db/schema.ts",

65ce77b374cead4ac350798cd227a2815fa05174
austingw <senortinman@gmail.com>
2024-02-25 01:06:34 -0500

    Add insert functions to createPost server action (and it works 🤯)

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -1,21 +1,48 @@
 "use server";
 
 import { getPresignedUrl } from "@/utils/getPresignedUrl";
+import { insertAudio } from "@/utils/db/audioDbOperations";
+import { insertPost } from "@/utils/db/postDbOperations";
 
 export const createPost = async (prevState: any, post: FormData) => {
-  const title = post.get("title");
+  const title = String(post.get("title"));
+  const description = String(post.get("description"));
   const file = post.get("file");
 
+  let audioId;
   if (file) {
-    const presignedUrl = await getPresignedUrl();
-    const res = await fetch(presignedUrl, {
-      method: "PUT",
-      body: file,
-      headers: {
-        "Content-Type": "audio/mp3", //to be dynamic based on file type
-        "Content-Disposition": `attachment; filename="${title}"`,
-      },
+    try {
+      const presignedUrl = await getPresignedUrl();
+      const res = await fetch(presignedUrl, {
+        method: "PUT",
+        body: file,
+        headers: {
+          "Content-Type": "audio/mp3", //to be dynamic based on file type
+          "Content-Disposition": `attachment; filename="${title}"`,
+        },
+      });
+      const audioUrl = res.url.split("?")[0];
+
+      audioId = await insertAudio({
+        url: audioUrl,
+        profileId: "1",
+      });
+      console.log(audioId);
+    } catch {
+      throw new Error("There was an error uploading the file");
+    }
+  }
+
+  try {
+    await insertPost({
+      title,
+      description,
+      type: "demo",
+      published: true,
+      profileId: "1",
+      audioId: audioId ? audioId[0].id : null,
     });
-    const audioUrl = res.url.split("?")[0];
+  } catch {
+    throw new Error("There was an error creating the post");
   }
 };

e3d12033054d1419ea40b9b132f693a540cc4b2e
austingw <senortinman@gmail.com>
2024-02-25 21:08:24 -0500

    Add post query operation

--- src/utils/db/audioDbOperations.ts
+++ src/utils/db/audioDbOperations.ts
@@ -4,7 +4,7 @@ import { audio } from "@/db/schema";
 type NewAudio = typeof audio.$inferInsert;
 
 export const insertAudio = async (newAudio: NewAudio) => {
-  return db.insert(audio).values(newAudio).returning({
+  return await db.insert(audio).values(newAudio).returning({
     id: audio.id,
   });
 };
--- src/utils/db/postDbOperations.ts
+++ src/utils/db/postDbOperations.ts
@@ -1,8 +1,22 @@
 import { db } from "@/db/db";
 import { posts } from "@/db/schema";
+import { desc } from "drizzle-orm";
 
 type NewPost = typeof posts.$inferInsert;
 
 export const insertPost = async (post: NewPost) => {
-  return db.insert(posts).values(post);
+  return await db.insert(posts).values(post);
+};
+
+export const queryPosts = async () => {
+  return await db.query.posts.findMany({
+    with: {
+      audio: true,
+      images: true,
+      profile: true,
+    },
+    limit: 10,
+    offset: 0,
+    orderBy: [desc(posts.createdAt)],
+  });
 };

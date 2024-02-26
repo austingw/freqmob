237df605b94d055df38e36951fe2008b949c1de0
austingw <senortinman@gmail.com>
2024-02-25 21:08:46 -0500

    Add schema to db instance for query

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,9 +1,12 @@
 import { drizzle } from "drizzle-orm/libsql";
 import { createClient } from "@libsql/client";
+import * as schema from "./schema";
 
 export const client = createClient({
   url: process.env.DATABASE_URL!,
   authToken: process.env.DATABASE_AUTH_TOKEN,
 });
 
-export const db = drizzle(client);
+export const db = drizzle(client, {
+  schema,
+});

4dd3ff7772c0fc3c8cd40d69ff785f2c25fa8e15
austingw <senortinman@gmail.com>
2024-03-03 13:25:05 -0500

    Update auth file to get signup form going

--- src/db/auth.ts
+++ src/db/auth.ts
@@ -12,11 +12,21 @@ export const lucia = new Lucia(adapter, {
       secure: process.env.NODE_ENV === "production",
     },
   },
+  getUserAttributes: (attributes) => {
+    return {
+      // attributes has the type of DatabaseUserAttributes
+      username: attributes.username,
+    };
+  },
 });
 
 // IMPORTANT!
 declare module "lucia" {
   interface Register {
     Lucia: typeof lucia;
+    DatabaseUserAttributes: DatabaseUserAttributes;
   }
+}
+interface DatabaseUserAttributes {
+  username: string;
 }

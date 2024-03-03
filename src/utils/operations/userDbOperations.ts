8d0a4e40b59ef9653735dfcf306929975d129328
austingw <senortinman@gmail.com>
2024-03-03 13:25:32 -0500

    Add user db oeprations file

---
+++ src/utils/operations/userDbOperations.ts
@@ -0,0 +1,23 @@
+import { db } from "@/db/db";
+import { profiles, userTable } from "@/db/schema";
+import { asc, desc, eq } from "drizzle-orm";
+
+type NewUser = typeof userTable.$inferInsert;
+type NewProfile = typeof profiles.$inferInsert;
+
+export const insertUser = async (user: NewUser) => {
+  return await db.insert(userTable).values(user).returning({
+    id: userTable.id,
+  });
+};
+
+export const insertProfile = async (profile: NewProfile) => {
+  return await db.insert(profiles).values(profile);
+};
+
+export const checkUsername = async (username: string) => {
+  return await db
+    .select()
+    .from(userTable)
+    .where(eq(userTable.username, username));
+};

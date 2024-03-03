3d53ff8a3e711dab1a182e303a76e0bd0d3c2131
austingw <senortinman@gmail.com>
2024-03-02 23:55:36 -0500

    Remove unnecessary console.log

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,8 @@ import AudioPlayer from "@/components/AudioPlayer";
 export default async function Home() {
   const posts = await getPosts();
 
+  console.log(posts);
+
   return (
     <main className={styles.main}>
       <div className={styles.hero}>

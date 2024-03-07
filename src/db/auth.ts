1b8ac740cb5ee0adead40e27f9aa510569655b2c
austingw <senortinman@gmail.com>
2024-03-06 21:01:18 -0500

    Add validateUser method to use for authorizing user actions

--- src/db/auth.ts
+++ src/db/auth.ts
@@ -1,7 +1,9 @@
-import { Lucia } from "lucia";
+import { Lucia, Session, User } from "lucia";
 import { db } from "./db";
 import { DrizzleSQLiteAdapter } from "@lucia-auth/adapter-drizzle";
 import { sessionTable, userTable } from "./schema";
+import { cookies } from "next/headers";
+import { cache } from "react";
 
 const adapter = new DrizzleSQLiteAdapter(db, sessionTable, userTable);
 
@@ -19,6 +21,42 @@ export const lucia = new Lucia(adapter, {
     };
   },
 });
+
+export const validateRequest = cache(
+  async (): Promise<
+    { user: User; session: Session } | { user: null; session: null }
+  > => {
+    const sessionId = cookies().get(lucia.sessionCookieName)?.value ?? null;
+    if (!sessionId) {
+      return {
+        user: null,
+        session: null,
+      };
+    }
+
+    const result = await lucia.validateSession(sessionId);
+    // next.js throws when you attempt to set cookie when rendering page
+    try {
+      if (result.session && result.session.fresh) {
+        const sessionCookie = lucia.createSessionCookie(result.session.id);
+        cookies().set(
+          sessionCookie.name,
+          sessionCookie.value,
+          sessionCookie.attributes
+        );
+      }
+      if (!result.session) {
+        const sessionCookie = lucia.createBlankSessionCookie();
+        cookies().set(
+          sessionCookie.name,
+          sessionCookie.value,
+          sessionCookie.attributes
+        );
+      }
+    } catch {}
+    return result;
+  }
+);
 
 // IMPORTANT!
 declare module "lucia" {

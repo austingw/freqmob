7a80bad530c36ca562dd3474ceaab9a654fe42d6
austingw <senortinman@gmail.com>
2024-03-29 23:40:32 -0400

    Get that stuff working on actualy page

--- src/app/fm/[board]/page.tsx
+++ src/app/fm/[board]/page.tsx
@@ -1,4 +1,22 @@
-export default function Page({ params }: { params: { board: string } }) {
-	console.log(params.board)
-	return <div>My Post: {params.board}</div>
+"use server";
+import Feed from "@/components/Feed";
+import { queryBoardByName } from "@/utils/operations/boardDbOperations";
+import { queryPostsByBoard } from "@/utils/operations/postDbOperations";
+
+export default async function Page({ params }: { params: { board: string } }) {
+  const boardData = await queryBoardByName(params.board);
+  if (!boardData) {
+    return {
+      status: 404,
+      body: "Not found",
+    };
+  }
+  const posts = await queryPostsByBoard(boardData[0].id);
+  console.log(boardData);
+  console.log(typeof posts);
+  return (
+    <div>
+      My Post: {boardData[0].name}
+    </div>
+  );
 }

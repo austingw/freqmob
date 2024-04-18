a45ce3448a4bf3ff36f9b2eaf06c50ba6e29d003
austingw <senortinman@gmail.com>
2024-04-17 21:48:09 -0400

    Add comment operations

---
+++ src/utils/operations/commentDbOperations.ts
@@ -0,0 +1,26 @@
+import { db } from "@/db/db";
+import { comments, posts } from "@/db/schema";
+import { desc, eq } from "drizzle-orm";
+
+type NewComment = typeof comments.$inferInsert;
+
+export const insertComment = async (comment: NewComment) => {
+  return await db.insert(comments).values(comment);
+};
+
+export const queryComments = async (postId: string) => {
+  return await db
+    .select()
+    .from(comments)
+    .where(eq(comments.postId, postId))
+    .orderBy(desc(comments.createdAt));
+};
+
+export const queryCommentsByUser = async (profileId: string) => {
+  return await db
+    .select()
+    .from(comments)
+    .where(eq(comments.profileId, profileId))
+    .innerJoin(posts, eq(posts.id, comments.postId))
+    .orderBy(desc(comments.createdAt));
+};

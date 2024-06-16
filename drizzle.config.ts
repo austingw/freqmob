2e4e22042f9365d7311818a3ec2e2342077c1079
austingw <senortinman@gmail.com>
2024-06-16 12:24:01 -0400

    Update drizz config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -7,10 +7,13 @@ dotenv.config({
 
 export default defineConfig({
   schema: "./src/db/schema.ts",
+  dialect: "sqlite",
   out: "./drizzle",
   driver: "turso",
   dbCredentials: {
     url: process.env.DATABASE_URL!,
     authToken: process.env.DATABASE_AUTH_TOKEN,
   },
+  verbose: true,
+  strict: true,
 });

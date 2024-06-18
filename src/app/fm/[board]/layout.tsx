16a835f4aed23e01131500afd7b738e4f9890372
austingw <senortinman@gmail.com>
2024-06-17 22:41:47 -0400

    Unify layout for /fm/ subpages

--- src/app/fm/[board]/layout.tsx
+++
@@ -1,7 +0,0 @@
-export default function BoardLayout({
-	children,
-}: {
-	children: React.ReactNode
-}) {
-	return <section>{children}</section>
-}
--- src/app/fm/[board]/page.tsx
+++ src/app/fm/[board]/page.tsx
@@ -1,8 +1,14 @@
 "use server";
 
 import Feed from "@/components/Feed";
+import { validateRequest } from "@/db/auth";
 import { queryBoardByName } from "@/utils/operations/boardDbOperations";
 import { queryPostsByBoard } from "@/utils/operations/postDbOperations";
+import {
+  addBoardSub,
+  getProfileFromUserId,
+  removeBoardSub,
+} from "@/utils/operations/userDbOperations";
 
 export default async function Page({ params }: { params: { board: string } }) {
   const boardData = await queryBoardByName(params.board);
@@ -12,6 +18,22 @@ export default async function Page({ params }: { params: { board: string } }) {
       body: "Not found",
     };
   }
+
+  const user = await validateRequest();
+
+  const profile = user.user ? await getProfileFromUserId(user.user.id) : null;
+  const joinBoard = async () => {
+    if (profile && profile[0].id) {
+      await addBoardSub(profile[0].id, boardData[0].name);
+    }
+  };
+
+  const leaveBoard = async () => {
+    if (profile && profile[0].id) {
+      await removeBoardSub(profile[0].id, boardData[0].name);
+    }
+  };
+
   const posts = boardData[0]?.id
     ? await queryPostsByBoard(1, boardData[0].id)
     : null;

1a69bed790afd23316c9a15773607617fd8b1610
austingw <senortinman@gmail.com>
2024-06-27 22:28:35 -0400

    Setup hook for post like

---
+++ src/queries/likes.ts
@@ -0,0 +1,9 @@
+import { getUserLike } from "@/app/actions";
+import { useQuery } from "@tanstack/react-query";
+
+export const useGetUserLike = (postId: number, profileId: string) => {
+  return useQuery({
+    queryKey: ["userLikes", postId, profileId],
+    queryFn: () => getUserLike(String(postId), profileId),
+  });
+};

0ae350e8d96406ed9cbda4c166fa3ad8514041aa
austingw <senortinman@gmail.com>
2024-06-29 23:51:37 -0400

    Add queries for like stuff

--- src/queries/likes.ts
+++ src/queries/likes.ts
@@ -1,9 +1,27 @@
-import { getUserLike } from "@/app/actions";
-import { useQuery } from "@tanstack/react-query";
+import { getLikeCount, getUserLike } from "@/app/actions";
+import { UserLike } from "@/types/userTypes";
+import { keepPreviousData, useQuery } from "@tanstack/react-query";
 
-export const useGetUserLike = (postId: number, profileId: string) => {
+export const useGetUserLike = (
+  postId: number,
+  profileId: string,
+  initialLike: UserLike | null,
+) => {
   return useQuery({
-    queryKey: ["userLikes", postId, profileId],
+    queryKey: ["userLike", postId, profileId],
     queryFn: () => getUserLike(postId, profileId),
+    initialData: initialLike,
+    staleTime: 0,
+    placeholderData: keepPreviousData,
+  });
+};
+
+export const useGetLikeCount = (postId: number, initialCount: number) => {
+  return useQuery({
+    queryKey: ["likeCount", postId],
+    queryFn: () => getLikeCount(postId),
+    initialData: initialCount,
+    staleTime: 0,
+    placeholderData: keepPreviousData,
   });
 };

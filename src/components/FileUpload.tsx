a2c4ee6ec5bd9cc2cc97743b7c67a741ccc393e6
austingw <senortinman@gmail.com>
2024-07-01 22:48:27 -0400

    Add state for when a file is successfully uploaded

--- src/components/FileUpload.tsx
+++ src/components/FileUpload.tsx
@@ -1,9 +1,14 @@
 "use client";
 
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { Text, Group, Button, rem, useMantineTheme } from "@mantine/core";
 import { Dropzone } from "@mantine/dropzone";
-import { IconCloudUpload, IconX, IconDownload } from "@tabler/icons-react";
+import {
+  IconCloudUpload,
+  IconX,
+  IconDownload,
+  IconCheck,
+} from "@tabler/icons-react";
 import classes from "./FileUpload.module.css";
 
 type FileUploadProps = {
@@ -13,6 +18,7 @@ type FileUploadProps = {
 const FileUpload = ({ addFile }: FileUploadProps) => {
   const theme = useMantineTheme();
   const openRef = useRef<() => void>(null);
+  const [validFile, setValidFile] = useState(false);
 
   return (
     <div className={classes.wrapper}>
@@ -22,47 +28,68 @@ const FileUpload = ({ addFile }: FileUploadProps) => {
         radius="md"
         onDrop={(files) => {
           addFile(files[0]);
+          setValidFile(true);
         }}
         maxSize={30 * 1024 ** 2}
       >
         <div style={{ pointerEvents: "none" }}>
           <Group justify="center">
-            <Dropzone.Accept>
-              <IconDownload
-                style={{ width: rem(50), height: rem(50) }}
-                color={theme.colors.blue[6]}
-                stroke={1.5}
-              />
-            </Dropzone.Accept>
-            <Dropzone.Reject>
-              <IconX
-                style={{ width: rem(50), height: rem(50) }}
-                color={theme.colors.red[6]}
-                stroke={1.5}
-              />
-            </Dropzone.Reject>
-            <Dropzone.Idle>
-              <IconCloudUpload
+            {validFile ? (
+              <IconCheck
                 style={{ width: rem(50), height: rem(50) }}
                 stroke={1.5}
               />
-            </Dropzone.Idle>
+            ) : (
+              <>
+                <Dropzone.Accept>
+                  <IconDownload
+                    style={{ width: rem(50), height: rem(50) }}
+                    color={theme.colors.blue[6]}
+                    stroke={1.5}
+                  />
+                </Dropzone.Accept>
+                <Dropzone.Reject>
+                  <IconX
+                    style={{ width: rem(50), height: rem(50) }}
+                    color={theme.colors.red[6]}
+                    stroke={1.5}
+                  />
+                </Dropzone.Reject>
+                <Dropzone.Idle>
+                  <IconCloudUpload
+                    style={{ width: rem(50), height: rem(50) }}
+                    stroke={1.5}
+                  />
+                </Dropzone.Idle>
+              </>
+            )}
           </Group>
 
-          <Text ta="center" fw={700} fz="lg" mt="xl" c={theme.primaryColor}>
-            <Dropzone.Accept>Drop file here</Dropzone.Accept>
-            <Dropzone.Reject>Audio file must be less than 30mb</Dropzone.Reject>
-            <Dropzone.Idle>Upload audio</Dropzone.Idle>
-          </Text>
-          <Text ta="center" fz="sm" mt="xs" c="dimmed">
-            Drag files here or use the button below to upload. We can accept
-            only <i>.wav, .mp3, .flac, .acc, and .ogg</i> files that are less
-            than 30mb in size.
-          </Text>
+          {validFile ? (
+            <Text ta="center" fw={700} fz="lg" c={theme.primaryColor}>
+              {" "}
+              File Ready!
+            </Text>
+          ) : (
+            <>
+              <Text ta="center" fw={700} fz="lg" c={theme.primaryColor}>
+                <Dropzone.Accept>Drop file here</Dropzone.Accept>
+                <Dropzone.Reject>
+                  Audio file must be less than 30mb
+                </Dropzone.Reject>
+                <Dropzone.Idle>Upload audio</Dropzone.Idle>
+              </Text>
+              <Text ta="center" fz="sm" mt="xs" c="dimmed">
+                Drag files here or use the button below to upload. We can accept
+                only <i>.wav, .mp3, .flac, .acc, and .ogg</i> files that are
+                less than 30mb in size.
+              </Text>
+            </>
+          )}
         </div>
       </Dropzone>
 
-      <Button variant="filled" size="xl" onClick={() => openRef.current?.()}>
+      <Button variant="filled" size="lg" onClick={() => openRef.current?.()}>
         Select files
       </Button>
     </div>

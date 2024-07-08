433ef42f5d753db376bf782bdaca49556d8ae7a0
austingw <senortinman@gmail.com>
2024-07-07 23:11:22 -0400

    Fix board list refetching behavior

--- src/queries/boards.ts
+++ src/queries/boards.ts
@@ -1,9 +1,11 @@
 import { getUserBoardList } from "@/app/actions/boardActions";
-import { useQuery } from "@tanstack/react-query";
+import { keepPreviousData, useQuery } from "@tanstack/react-query";
 
 export const useGetBoardList = (profileId: string) => {
   return useQuery({
     queryKey: ["boardList", profileId],
     queryFn: () => getUserBoardList(profileId),
+    staleTime: 0,
+    placeholderData: keepPreviousData,
   });
 };

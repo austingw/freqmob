320eae3c13277b0ffa839d3d14d8422653d53767
austingw <senortinman@gmail.com>
2024-07-08 22:30:02 -0400

    Initial data for profile page

--- src/app/u/[username]/page.tsx
+++ src/app/u/[username]/page.tsx
@@ -1,10 +1,32 @@
 "use server";
 
+import { queryCommentsByProfile } from "@/utils/operations/commentDbOperations";
+import { queryPostsByProfile } from "@/utils/operations/postDbOperations";
+import { getProfileFromUsername } from "@/utils/operations/userDbOperations";
+
 export default async function Page({
   params,
 }: {
   params: { username: string };
 }) {
-  console.log(params.username);
-  return <div></div>;
+  const profile = await getProfileFromUsername(params?.username);
+
+  if (!profile[0]?.name) {
+    return (
+      <div>
+        <h1>User not found</h1>
+      </div>
+    );
+  }
+
+  const posts = await queryPostsByProfile(profile[0]?.id);
+  const comments = await queryCommentsByProfile(profile[0]?.id);
+
+  console.log(profile, posts, comments);
+
+  return (
+    <div>
+      <h1>{profile[0]?.name}</h1>
+    </div>
+  );
 }

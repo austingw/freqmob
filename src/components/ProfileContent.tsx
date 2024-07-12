92c546d7b792b4da7f745ef32b7aa9b5d1d9f573
austingw <senortinman@gmail.com>
2024-07-12 00:04:28 -0400

    Add media query

--- src/components/ProfileContent.tsx
+++ src/components/ProfileContent.tsx
@@ -1,7 +1,8 @@
 "use client";
 
 import { CommentWithPost, PostWithAudio, profiles } from "@/db/schema";
-import { SegmentedControl, Stack } from "@mantine/core";
+import { SegmentedControl, Stack, useMantineTheme } from "@mantine/core";
+import { useMediaQuery } from "@mantine/hooks";
 import { useState } from "react";
 
 type Segment = "details" | "posts" | "comments";
@@ -13,11 +14,15 @@ interface ProfileContentProps {
 }
 
 const ProfileContent = ({ profile, posts, comments }: ProfileContentProps) => {
+  const theme = useMantineTheme();
+  const isMobile = useMediaQuery("(max-width: 768px)");
   const [segment, setSegment] = useState<Segment>("details");
   console.log("ppppp", profile);
   return (
     <Stack>
       <SegmentedControl
+        color={theme.primaryColor}
+        w={isMobile ? "100%" : "300px"}
         data={["details", "posts", "comments"]}
         value={segment}
         onChange={(value) => setSegment(value as Segment)}

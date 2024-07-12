c03f1ecf06ff42eeaee33759b7ffc5519bcc11b4
austingw <senortinman@gmail.com>
2024-07-11 23:44:43 -0400

    Add edit button for profile header

--- src/components/ProfileHeader.tsx
+++ src/components/ProfileHeader.tsx
@@ -1,14 +1,21 @@
+"use client";
+
 import { profiles } from "@/db/schema";
+import { useAtomValue } from "jotai";
+import { profileAtom } from "./FMAppShell";
+import { Button, Group } from "@mantine/core";
 
 const ProfileHeader = ({
   profile,
 }: {
   profile: typeof profiles.$inferSelect;
 }) => {
+  const profileValue = useAtomValue(profileAtom);
   return (
-    <div>
+    <Group align="center" justify="space-between">
       <h1>{profile?.name || "User not found"}</h1>
-    </div>
+      {profileValue?.id === profile?.id && <Button>Edit</Button>}
+    </Group>
   );
 };
 

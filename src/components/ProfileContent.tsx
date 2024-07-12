da948da5325416dc3652273a66dfa6cd8a6d5600
austingw <senortinman@gmail.com>
2024-07-11 23:43:11 -0400

    Basic setup for profile content

--- src/components/ProfileContent.tsx
+++ src/components/ProfileContent.tsx
@@ -1,18 +1,28 @@
+"use client";
+
 import { comments, posts, profiles } from "@/db/schema";
 import { SegmentedControl, Stack } from "@mantine/core";
+import { useState } from "react";
 
 interface ProfileContentProps {
   profile: typeof profiles.$inferSelect;
-  posts: typeof posts.$inferSelect;
-  comments: typeof comments.$inferSelect;
+  posts: (typeof posts.$inferSelect)[];
+  comments: (typeof comments.$inferSelect)[];
 }
 
 const ProfileContent = ({ profile, posts, comments }: ProfileContentProps) => {
-  console.log("test", profile, posts, comments);
+  const [segment, setSegment] = useState<string>("details");
+  console.log("ppppp", profile);
   return (
     <Stack>
-      <SegmentedControl data={["Details", "Posts", "Comments"]} />
-      <Stack></Stack>
+      <SegmentedControl
+        data={["details", "posts", "comments"]}
+        value={segment}
+        onChange={(value) => setSegment(value)}
+      />
+      {segment === "details" && <Stack>{String(profile)}</Stack>}
+      {segment === "posts" && <Stack>{String(posts)}</Stack>}
+      {segment === "comments" && <Stack>{String(comments)}</Stack>}
     </Stack>
   );
 };

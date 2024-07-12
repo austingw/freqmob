6c78c0bf0d1d77add0a63ce46a501a7789875a9d
austingw <senortinman@gmail.com>
2024-07-11 23:49:02 -0400

    Add type for segments that is slightly better

--- src/components/ProfileContent.tsx
+++ src/components/ProfileContent.tsx
@@ -4,6 +4,8 @@ import { comments, posts, profiles } from "@/db/schema";
 import { SegmentedControl, Stack } from "@mantine/core";
 import { useState } from "react";
 
+type Segment = "details" | "posts" | "comments";
+
 interface ProfileContentProps {
   profile: typeof profiles.$inferSelect;
   posts: (typeof posts.$inferSelect)[];
@@ -11,14 +13,14 @@ interface ProfileContentProps {
 }
 
 const ProfileContent = ({ profile, posts, comments }: ProfileContentProps) => {
-  const [segment, setSegment] = useState<string>("details");
+  const [segment, setSegment] = useState<Segment>("details");
   console.log("ppppp", profile);
   return (
     <Stack>
       <SegmentedControl
         data={["details", "posts", "comments"]}
         value={segment}
-        onChange={(value) => setSegment(value)}
+        onChange={(value) => setSegment(value as Segment)}
       />
       {segment === "details" && <Stack>{String(profile)}</Stack>}
       {segment === "posts" && <Stack>{String(posts)}</Stack>}

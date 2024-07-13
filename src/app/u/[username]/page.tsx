e3a213df62b0d8fa78810832807f45a06e86ebb8
austingw <senortinman@gmail.com>
2024-07-12 21:55:08 -0400

    Add additional info for profile data

--- src/app/u/[username]/page.tsx
+++ src/app/u/[username]/page.tsx
@@ -1,10 +1,15 @@
 "use server";
 
+import { getUserLikes } from "@/app/actions/likeActions";
 import ProfileContent from "@/components/ProfileContent";
 import ProfileHeader from "@/components/ProfileHeader";
+import { validateRequest } from "@/db/auth";
 import { queryCommentsByProfile } from "@/utils/operations/commentDbOperations";
 import { queryPostsByProfile } from "@/utils/operations/postDbOperations";
-import { getProfileFromUsername } from "@/utils/operations/userDbOperations";
+import {
+  getProfileFromUserId,
+  getProfileFromUsername,
+} from "@/utils/operations/userDbOperations";
 
 export default async function Page({
   params,
@@ -24,12 +29,28 @@ export default async function Page({
   const posts = await queryPostsByProfile(profile[0]?.id);
   const comments = await queryCommentsByProfile(profile[0]?.id);
 
+  const postIds = posts ? posts.map((post) => post.posts.id) : [];
+
+  //call for the user that is logged in
+  const user = await validateRequest();
+  const userProfile = user.user && (await getProfileFromUserId(user.user.id));
+  const userProfileId = userProfile ? userProfile[0].id : null;
+  const postLikes =
+    postIds.length > 0 && userProfileId
+      ? await getUserLikes(postIds, userProfileId)
+      : null;
+
   console.log(profile, posts, comments);
 
   return (
     <div>
       <ProfileHeader profile={profile[0]} />
-      <ProfileContent profile={profile[0]} posts={posts} comments={comments} />
+      <ProfileContent
+        profile={profile[0]}
+        posts={posts}
+        comments={comments}
+        initialLikes={postLikes}
+      />
     </div>
   );
 }

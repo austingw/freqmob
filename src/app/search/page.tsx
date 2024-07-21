36dc25323785039a4a2bd2747cb6b33afe0f5b72
austingw <senortinman@gmail.com>
2024-07-21 14:02:46 -0400

    Add initial posts/board list get calls to server component

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,7 +1,14 @@
+import { queryBoardsBySearchTerm } from "@/utils/operations/boardDbOperations";
+import { queryPostsBySearchTerm } from "@/utils/operations/postDbOperations";
+
 export default async function Page({
   searchParams,
 }: {
   searchParams: { term: string };
 }) {
+  const posts = await queryPostsBySearchTerm(searchParams.term, 1, "new");
+  const boards = await queryBoardsBySearchTerm(searchParams.term);
+
+  console.log(posts, boards);
   return <div>Search Results {searchParams.term}</div>;
 }

12fc89792efdf10182318b7714aa7214d6ee4d8f
austingw <senortinman@gmail.com>
2024-07-21 15:19:23 -0400

    Pass initial likes to search results

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,6 +1,9 @@
 import SearchResults from "@/components/SearchResults";
+import { validateRequest } from "@/db/auth";
 import { queryBoardsBySearchTerm } from "@/utils/operations/boardDbOperations";
 import { queryPostsBySearchTerm } from "@/utils/operations/postDbOperations";
+import { getProfileFromUserId } from "@/utils/operations/userDbOperations";
+import { getUserLikes } from "../actions/likeActions";
 
 export default async function Page({
   searchParams,
@@ -10,11 +13,20 @@ export default async function Page({
   const posts = await queryPostsBySearchTerm(searchParams.term, 1, "new");
   const boards = await queryBoardsBySearchTerm(searchParams.term);
 
+  const user = await validateRequest();
+  const postIds = posts ? posts.map((post) => post.posts.id) : [];
+  const profile = user.user && (await getProfileFromUserId(user.user.id));
+  const profileId = profile ? profile[0].id : null;
+  const postLikes =
+    postIds.length > 0 && profileId
+      ? await getUserLikes(postIds, profileId)
+      : null;
+
   return (
     <SearchResults
       initialPosts={posts}
       initialBoards={boards}
-      initialLikes={null}
+      initialLikes={postLikes}
       count={10}
       searchTerm={searchParams.term}
     />

9d12cde2c3c51caacedcacc2f06fe638300623fd
austingw <senortinman@gmail.com>
2024-07-21 14:03:15 -0400

    Make hook for posts by search term

--- src/queries/posts.ts
+++ src/queries/posts.ts
@@ -1,4 +1,7 @@
-import { getPostsByBoard } from "@/app/actions/postActions";
+import {
+  getPostsByBoard,
+  getPostsBySearchTerm,
+} from "@/app/actions/postActions";
 import { PostWithMedia } from "@/db/schema";
 import { keepPreviousData, useQuery } from "@tanstack/react-query";
 
@@ -11,6 +14,21 @@ export const useGetPosts = (
   return useQuery({
     queryKey: ["posts", boardId, page, sort],
     queryFn: () => getPostsByBoard(page, sort, Number(boardId)),
+    initialData: initialPosts,
+    staleTime: 0,
+    placeholderData: keepPreviousData,
+  });
+};
+
+export const useGetPostsBySearchTerm = (
+  initialPosts: PostWithMedia[] | null,
+  searchTerm: string,
+  page: number,
+  sort: SortOptions,
+) => {
+  return useQuery({
+    queryKey: ["posts", searchTerm, page, sort],
+    queryFn: () => getPostsBySearchTerm(searchTerm, page, sort),
     initialData: initialPosts,
     staleTime: 0,
     placeholderData: keepPreviousData,

03a59116195b9907df38d510cd13dfdc318b591e
austingw <senortinman@gmail.com>
2024-07-30 09:51:13 -0400

    Add notification operations

---
+++ src/utils/operations/notificationDbOperations.ts
@@ -0,0 +1,28 @@
+import { db } from "@/db/db";
+import { notifications } from "@/db/schema";
+import { desc, eq } from "drizzle-orm";
+
+type NewNotification = typeof notifications.$inferInsert;
+
+export const insertNotification = async (notification: NewNotification) => {
+  await db.insert(notifications).values(notification);
+};
+
+export const updateNotificationAsRead = async (id: number) => {
+  await db
+    .update(notifications)
+    .set({ isRead: true })
+    .where(eq(notifications.id, id));
+};
+
+export const deleteNotification = async (id: number) => {
+  await db.delete(notifications).where(eq(notifications.id, id));
+};
+
+export const queryUserNotifications = async (profileId: string) => {
+  await db
+    .select()
+    .from(notifications)
+    .where(eq(notifications.profileId, profileId))
+    .orderBy(desc(notifications.createdAt));
+};

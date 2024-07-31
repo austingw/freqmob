0e9f9b07364b69d0ff0b3be6a7a57c9e82d4b452
austingw <senortinman@gmail.com>
2024-07-31 17:52:09 -0400

    Add post notification action

--- src/app/actions/notificationActions.ts
+++ src/app/actions/notificationActions.ts
@@ -1,5 +1,6 @@
 "use server";
 
+import { queryBoardById } from "@/utils/operations/boardDbOperations";
 import { insertNotification } from "@/utils/operations/notificationDbOperations";
 import { queryPostDetailsById } from "@/utils/operations/postDbOperations";
 
@@ -16,6 +17,28 @@ export const postCommentNotification = async (
         postId,
         content: `u/${username} commented on your post "${postDetails[0].title}"`,
         profileId: postDetails[0].profileId,
+      });
+    } catch (e) {
+      console.error(e);
+    }
+  }
+};
+
+export const postPostNotification = async (
+  boardId: number,
+  postTitle: number,
+  postId: number,
+  posterId: string,
+  username: string,
+) => {
+  const boardDetails = await queryBoardById(boardId);
+  if (posterId !== boardDetails[0].profileId) {
+    try {
+      await insertNotification({
+        boardId,
+        content: `New post ${postTitle} by u/${username} posted in your board "${boardDetails[0].name}"`,
+        postId,
+        profileId: boardDetails[0].profileId,
       });
     } catch (e) {
       console.error(e);

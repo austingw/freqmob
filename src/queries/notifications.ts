364376f0ab12ccfb7f3aee5f46a5420a4203b675
austingw <senortinman@gmail.com>
2024-08-01 23:44:32 -0400

    Add query hook for notifications

---
+++ src/queries/notifications.ts
@@ -0,0 +1,11 @@
+import { getNotifications } from "@/app/actions/notificationActions";
+import { keepPreviousData, useQuery } from "@tanstack/react-query";
+
+export const useGetNotifications = (profileId: string, page: number) => {
+  return useQuery({
+    queryKey: ["notifications", profileId, page],
+    queryFn: () => getNotifications(profileId, page),
+    staleTime: 0,
+    placeholderData: keepPreviousData,
+  });
+};

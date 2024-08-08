3cf01b5ef870c179439930d637e7bc0a21598203
austingw <senortinman@gmail.com>
2024-08-07 20:09:29 -0400

    Add setup for individual post pages (sharing)

---
+++ src/app/post/[id]/page.tsx
@@ -0,0 +1,22 @@
+import { getUserLikes } from "@/app/actions/likeActions";
+import PostWrapper from "@/components/PostWrapper";
+import { validateRequest } from "@/db/auth";
+import { queryPostById } from "@/utils/operations/postDbOperations";
+import { getProfileFromUserId } from "@/utils/operations/userDbOperations";
+
+export default async function Page({ params }: { params: { id: number } }) {
+  const post = await queryPostById(params?.id);
+  const user = await validateRequest();
+  const profile = user.user && (await getProfileFromUserId(user.user.id));
+  const profileId = profile ? profile[0].id : null;
+  const postLikes =
+    post && profileId
+      ? await getUserLikes([post?.[0]?.posts.id], profileId)
+      : null;
+
+  return (
+    <div>
+      <PostWrapper post={post?.[0]} userLike={postLikes?.[0] || null} />
+    </div>
+  );
+}

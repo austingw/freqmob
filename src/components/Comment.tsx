299b81bfa8ce835c99d15326cc398b997e78368e
austingw <senortinman@gmail.com>
2024-08-07 21:00:21 -0400

    Make the comment delete stuff work

--- src/components/Comment.tsx
+++ src/components/Comment.tsx
@@ -1,6 +1,7 @@
-import { CommentWithProfile, comments } from "@/db/schema";
+import { CommentWithProfile } from "@/db/schema";
 import formatDate from "@/utils/formatDate";
 import {
+  ActionIcon,
   Avatar,
   Button,
   Group,
@@ -9,35 +10,63 @@ import {
   Text,
   useMantineTheme,
 } from "@mantine/core";
+import { useAtomValue } from "jotai";
 import { useRouter } from "next/navigation";
+import { profileAtom } from "./FMAppShell";
+import { IconTrash } from "@tabler/icons-react";
+import { delComment } from "@/app/actions/commentActions";
+import { useQueryClient } from "@tanstack/react-query";
 
 const Comment = ({ comment }: { comment: CommentWithProfile }) => {
   const router = useRouter();
   const theme = useMantineTheme();
+  const queryClient = useQueryClient();
+
+  const profileValue = useAtomValue(profileAtom);
 
   return (
     <Paper withBorder radius="md" key={comment.comments.id} px={10}>
       <Stack gap={0} p={4} pb={12}>
-        <Group gap={4} align="center" justify="flex-start">
-          <Avatar
-            src={comment?.profiles?.avatar}
-            name={comment?.profiles?.name}
-            size={"sm"}
-            color={theme.primaryColor}
-          />
-          <Button
-            fz="xs"
-            variant="transparent"
-            p={0}
-            onClick={() => {
-              router.push(`/u/${comment.profiles.name}`);
-            }}
-          >
-            {comment.profiles.name}
-          </Button>
-          <Text fz="xs" c="dimmed">
-            - {formatDate(comment.comments.createdAt)}
-          </Text>
+        <Group align="center" justify="space-between">
+          <Group gap={4} align="center" justify="flex-start">
+            <Avatar
+              src={comment?.profiles?.avatar}
+              name={comment?.profiles?.name}
+              size={"sm"}
+              color={theme.primaryColor}
+            />
+            <Button
+              fz="xs"
+              variant="transparent"
+              p={0}
+              onClick={() => {
+                router.push(`/u/${comment.profiles.name}`);
+              }}
+            >
+              {comment.profiles.name}
+            </Button>
+            <Text fz="xs" c="dimmed">
+              - {formatDate(comment.comments.createdAt)}
+            </Text>
+          </Group>
+          {profileValue?.id === comment?.comments?.profileId && (
+            <ActionIcon
+              variant="subtle"
+              size={"xs"}
+              onClick={async () => {
+                await delComment(comment.comments.id).then(() => {
+                  queryClient.invalidateQueries({
+                    queryKey: ["comments", comment.comments.postId],
+                  });
+                  queryClient.invalidateQueries({
+                    queryKey: ["commentCount", comment.comments.postId],
+                  });
+                });
+              }}
+            >
+              <IconTrash />
+            </ActionIcon>
+          )}
         </Group>
         <Text fz="sm">{comment.comments.content}</Text>
       </Stack>

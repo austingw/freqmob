195807fc0c899091d0ee0a55ad0012e140ce012d
austingw <senortinman@gmail.com>
2024-08-08 20:55:00 -0400

    Add loading state for navigation

---
+++ src/app/loading.tsx
@@ -0,0 +1,11 @@
+import { Skeleton, Stack } from "@mantine/core";
+
+export default function Loading() {
+  return (
+    <Stack w="100%" h="100%" align="center">
+      <Skeleton height={50} width={"100%"} radius="xl" />
+      <Skeleton height={150} width={"100%"} radius="xl" />
+      <Skeleton height={250} width={"100%"} radius="xl" />
+    </Stack>
+  );
+}

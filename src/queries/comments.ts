38c0718a34c354da40a091f9288190b37b006307
austingw <senortinman@gmail.com>
2024-09-04 00:18:25 -0400

    Add new hook for comments

--- src/queries/comments.ts
+++ src/queries/comments.ts
@@ -1,10 +1,29 @@
-import { getCommentCount, getComments } from "@/app/actions/commentActions";
+import {
+  getCommentCount,
+  getComments,
+  getCommentsByProfile,
+} from "@/app/actions/commentActions";
+import { CommentWithPost } from "@/lib/db/schema";
 import { keepPreviousData, useQuery } from "@tanstack/react-query";
 
 export const useGetComments = (postId: number) => {
   return useQuery({
     queryKey: ["comments", postId],
     queryFn: () => getComments(postId),
+  });
+};
+
+export const useGetCommentsByProfile = (
+  initialComments: CommentWithPost[] | null,
+  page: number,
+  profileId: string,
+) => {
+  return useQuery({
+    queryKey: ["comments", profileId, page],
+    queryFn: () => getCommentsByProfile(page, profileId),
+    initialData: initialComments,
+    staleTime: 0,
+    placeholderData: keepPreviousData,
   });
 };
 

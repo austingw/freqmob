994e563d93fc6d632b5d1ed74244c148a12a8577
austingw <senortinman@gmail.com>
2024-09-05 21:55:03 -0400

    Create password reset email function

---
+++ src/lib/email/sendPasswordResetEmail.ts
@@ -0,0 +1,22 @@
+import mg from "./mg";
+
+export default async function sendPasswordResetEmail(
+  email: string,
+  verificationLink: string,
+) {
+  let res = false;
+  mg.messages
+    .create("mail.freqmob.com", {
+      from: "Freqmob <fm@mail.freqmob.com>",
+      to: [email],
+      subject: "Password Reset",
+      text: `Please use the following link to reset your password: ${verificationLink}`,
+      html: `<p>Please use the following link to reset your password: <a href="${verificationLink}">${verificationLink}</a></p>`,
+    })
+    .then(() => (res = true))
+    .catch((err) => {
+      res = false;
+      console.error(err);
+    });
+  return res;
+}

efb488334c9a0e506836ea36c6115ef3cb0aec3e
austingw <senortinman@gmail.com>
2024-09-07 01:43:30 -0400

    Add password server action

--- src/app/actions/authActions.ts
+++ src/app/actions/authActions.ts
@@ -7,14 +7,20 @@ import {
 } from "@/lib/auth/auth";
 import {
   checkUsername,
+  deletePasswordToken,
   insertProfile,
   insertUser,
+  queryPasswordToken,
+  updatePassword,
 } from "@/lib/db/operations/userDbOperations";
 import sendPasswordResetEmail from "@/lib/email/sendPasswordResetEmail";
 import { revalidatePath } from "next/cache";
 import { ActionResult } from "next/dist/server/app-render/types";
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
+import { isWithinExpirationDate } from "oslo";
+import { sha256 } from "oslo/crypto";
+import { encodeHex } from "oslo/encoding";
 import { Argon2id } from "oslo/password";
 
 export const signup = async (user: FormData) => {
@@ -176,4 +182,57 @@ export const sendPasswordResetToken = async (username: string) => {
   }
 
   return { message: "Email sent", status: 200 };
+};
+
+export const postNewPassword = async (
+  verificationToken: string,
+  input: FormData,
+) => {
+  const password = String(input.get("password"));
+  const password2 = String(input.get("password2"));
+
+  if (
+    typeof password !== "string" ||
+    password.length < 6 ||
+    password.length > 255
+  ) {
+    return {
+      message: "Invalid password",
+      status: 400,
+    };
+  }
+
+  if (password !== password2) {
+    return {
+      message: "Passwords do not match",
+      status: 400,
+    };
+  }
+
+  const tokenHash = encodeHex(
+    await sha256(new TextEncoder().encode(verificationToken)),
+  );
+  const token = await queryPasswordToken(tokenHash);
+  const expirationDate = new Date(token[0].expiresAt);
+
+  if (token[0]) {
+    await deletePasswordToken(tokenHash);
+  }
+
+  if (!token || !token[0] || !isWithinExpirationDate(expirationDate)) {
+    return { message: "Invalid token", status: 400 };
+  }
+
+  const hashedPassword = await new Argon2id().hash(password);
+
+  await updatePassword(token[0].userId, hashedPassword);
+
+  const session = await lucia.createSession(token[0].userId, {});
+  const sessionCookie = lucia.createSessionCookie(session.id);
+  cookies().set(
+    sessionCookie.name,
+    sessionCookie.value,
+    sessionCookie.attributes,
+  );
+  return { message: "Password updated", status: 200 };
 };
